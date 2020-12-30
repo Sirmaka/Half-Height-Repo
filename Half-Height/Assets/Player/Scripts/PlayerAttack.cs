484d4b6e646b300bfc7c72f307f54efa3a540088
Sirmaka <52619139+Sirmaka@users.noreply.github.com>
2020-12-30 14:57:47 +1100

    animation speed update, fixed animations for attacks

--- Half-Height/Assets/Player/Scripts/PlayerAttack.cs
+++ Half-Height/Assets/Player/Scripts/PlayerAttack.cs
@@ -8,21 +8,41 @@ public class PlayerAttack : MonoBehaviour
     public Transform attackPointLeft;
     private PlayerController playerController;
     private SpriteRenderer thisSpriteRenderer;
+    private Animator thisAnimator;  //  required for animation length
+    private AnimationClip clip; //  required for animation length
     public GameObject attackPrefab;
     private bool attacking;
-    public float attackAnimTime;
+    private float attackAnimTime;
+    private float airAttackAnimTime;
+    private float animationDuration;
+    // private float attackAnimTimeReset;
     public float attackCooldown;
     private bool onCooldown = false;   //  determines whether the attack is on cooldown
     private float attackCooldownReset;
-    private float attackAnimTimeReset;
+    
     private bool amAttacking = false;
     // Start is called before the first frame update
     void Start()
     {
         playerController = this.GetComponent<PlayerController>();
         thisSpriteRenderer = this.GetComponent<SpriteRenderer>();
-        attackAnimTimeReset = attackAnimTime;
+        
         attackCooldownReset = attackCooldown;
+        
+        // get the length of the attack animation, store it in attackAnimTime
+        thisAnimator = this.GetComponent<Animator>();
+        AnimationClip[] clips = thisAnimator.runtimeAnimatorController.animationClips;
+        foreach(AnimationClip clip in clips)
+        {
+            if(clip.name.Equals("attackAnim"))
+            {
+                attackAnimTime = clip.length;
+            }
+            if(clip.name.Equals("airAttackAnim"))
+            {
+                airAttackAnimTime = clip.length;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -52,8 +72,15 @@ public class PlayerAttack : MonoBehaviour
                 attack.transform.position = attackPointLeft.position;
                 attack.GetComponent<SpriteRenderer>().flipX = true;
             }
+            if(playerController.getGrounded())
+            {
+                animationDuration = attackAnimTime;
+            }
+            else
+            {
+                animationDuration = airAttackAnimTime;
+            }
             
-            attackAnimTime = attackAnimTimeReset;
             amAttacking = true;
             onCooldown = true;
             
@@ -72,14 +99,20 @@ public class PlayerAttack : MonoBehaviour
             onCooldown = false;
         }
 
+        /*
+            Animation alerts:
+                Attacking is a unique case. All action scripts must alert the PlayerAnimation script
+                when a change of animation state is required, but as attacking takes priority over all
+                other animations, the PlayerAttack script must alert PlayerAnimation when the attack begins
+                and ends.
+        */
         //animation timer
         if(amAttacking)
         {
-            attackAnimTime -= Time.deltaTime;
+            animationDuration -= Time.deltaTime;
         }
-
         //if attack animation is over, then alert PlayerController
-        if(attackAnimTime <= 0 && amAttacking)
+        if(animationDuration <= 0 && amAttacking)
         {
             amAttacking = false;
             playerController.setAttacking(false);
