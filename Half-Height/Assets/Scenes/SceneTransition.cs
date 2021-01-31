9128e787b9c1521c27f7c4a86c6d7652badbf01e
Sirmaka <52619139+Sirmaka@users.noreply.github.com>
2021-01-31 19:30:57 +1100

    created scene transition

---
+++ Half-Height/Assets/Scenes/SceneTransition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public LayerMask isPlayer;
+    private int room1 = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(isPlayer.Contains(collider.gameObject))
+        {
+            SceneManager.LoadScene(room1);
+        }
+    }
+}
